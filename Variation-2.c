431733063af2583b50f7be19f05376604a91c43f
WickedSalami <mleb.2200@gmail.com>
2025-08-27 22:23:53 +0800

    Added initialize

---
+++ Variation-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef struct{
+    int elem[MAX];
+    int count;
+}Etype, *EPtr;
+
+void initialize(EPtr L);
+void insertPos(EPtr L, int data, int position);
+void deletePos(EPtr L, int position);
+int locate(EPtr L, int data);
+int retrieve(EPtr L, int position);
+void insertSorted(EPtr L, int data);
+void display(EPtr L);
+void makeNULL(EPtr L);
+
+// Function definitions
+void initialize(EPtr L) {
+    L->count = 0;
+}
