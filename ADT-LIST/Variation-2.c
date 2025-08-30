bec08134ad4357ec959f14e5ae13637ba5e527b1
WickedSalami <mleb.2200@gmail.com>
2025-08-30 14:52:04 +0800

    updated

--- ADT-LIST/Variation-2.c
+++ ADT-LIST/Variation-2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 10
 
 typedef struct{
@@ -6,7 +7,7 @@ typedef struct{
     int count;
 }Etype, *EPtr;
 
-void initialize(EPtr L);
+void initialize(EPtr *L);
 void insertPos(EPtr L, int data, int index);
 void deletePos(EPtr L, int index);
 int locate(EPtr L, int data);
@@ -16,8 +17,8 @@ void display(EPtr L);
 void makeNULL(EPtr L);
 
 // Function definitions
-void initialize(EPtr L) {
-    L->count = 0;
+void initialize(EPtr *L) {
+    *L = (EPtr)calloc(1, sizeof(Etype));
 }
 
 void insertPos(EPtr L, int data, int index) {
@@ -47,24 +48,34 @@ int locate(EPtr L, int data) { // return the index of the search data
     return -1; // if not found
 }
 
-int retrieve(EPtr L, int index){
-    int i;
-        for(i = 0; i < L->count; i++) {
-            L->elem[i] == index;
-            return L->elem[i];
-        } 
+int retrieve(EPtr L, int index){ // pangitaon ang element based sa given index
+    // check if the index is valid
+    if(index < 0 || index >= L->count){
+        return -1; // if not valid
+    }
+    // return the element at the given index
+    return L->elem[index];
 }
 
 void display(EPtr L) {
     printf("[ ");
     for(int i = 0; i < L->count; i++) {
-        printf("%d ", L->elem[i]);
+        printf("%d, ", L->elem[i]);
     }
     printf("]\n");
 }
 
 void makeNULL(EPtr L) {
     L->count = 0;
+}
+
+void insertSorted(EPtr L, int data) {
+    int i;
+    for(i = L->count; i > 0 && L->elem[i-1] > data; i--) {
+        L->elem[i] = L->elem[i-1]; // copy the element to the free index to the right
+    }
+    L->elem[i] = data; // insert the data at the given index
+    L->count++;
 }
 
 int main() {
@@ -82,8 +93,12 @@ int main() {
 
     printf("Locating element 20... Index: %d\n", locate(list, 20)); // should print 2
 
+    printf("Inserting element 12 in sorted order...\n");
+    insertSorted(list, 12); // [10, 12, 20]
+    display(list);
+
     printf("Deleting element at index 1...\n");
-    deletePos(list, 1); // removes 15 → [10, 20]
+    deletePos(list, 1); // removes 15 -> [10, 20]
     display(list);
 
     printf("Making list empty...\n");
