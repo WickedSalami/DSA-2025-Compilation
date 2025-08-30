7a62917ef126cedc905ae8b114b43348e0d7e431
WickedSalami <mleb.2200@gmail.com>
2025-08-30 17:39:38 +0800

    updated variation 3

--- ADT-LIST/Variation-3.c
+++ ADT-LIST/Variation-3.c
@@ -24,14 +24,6 @@ List initialize(List L) {
     return L;
 }
 
-void display(List L){
-    printf("[ ");
-    for(int i = 0; i < L.count; i++) {
-        printf("%d, ", L.elemPtr[i]);
-    }
-    printf("]");
-}
-
 List insertPos(List L, int data, int index) {
     int i;
     if(index <= L.count) {
@@ -44,6 +36,45 @@ List insertPos(List L, int data, int index) {
     return L;
 }
 
+List deletePos(List L, int index) {
+    int i;
+    if(index < L.count) {
+        for(i = index; i < L.count; i++) {
+            L.elemPtr[i] = L.elemPtr[i+1];
+        }
+        L.count--;
+    }
+    return L;
+}
+
+int locate(List L, int data) {
+    int i;
+    for(i = 0; i < L.count; i++) {
+        if(L.elemPtr[i] == data) {
+            return i;
+        } 
+    }
+    return -1;
+}
+
+List insertSorted(List L, int data) {
+    int i;
+    for(i = L.count; i > 0 && L.elemPtr[i-1] > data; i--) { // if count is greater than 0 and the previous element is greater than data
+        L.elemPtr[i] = L.elemPtr[i-1]; // copies the element to the right
+    }
+    L.elemPtr[i] = data;
+    L.count++;
+    return L;
+}
+
+void display(List L){
+    printf("[ ");
+    for(int i = 0; i < L.count; i++) {
+        printf("%d, ", L.elemPtr[i]);
+    }
+    printf("]");
+}
+
 int main() {
     List myList;
 
@@ -51,7 +82,21 @@ int main() {
 
     myList = insertPos(myList, 1, 0);
     myList = insertPos(myList, 2, 1);
-    myList = insertPos(myList, 3, 2);
+    myList = insertPos(myList, 7, 2);
+    display(myList);
+
+    printf("\nAdded an element: ");
+    myList = insertPos(myList, 8, 3);
+    display(myList);
+
+    printf("\nLocating element 7... Index: %d", locate(myList, 7)); // should print 2
+
+    printf("\nInsert sorted: ");
+    myList = insertSorted(myList, 5);
+    display(myList);
+
+    printf("\nDeleted an element: ");
+    myList = deletePos(myList, 1);
     display(myList);
 
     return 0;
